3da2fb85ef693939bfa6988de1e43193b309eb50
Blackreaper9 <utm23090485@utma.edu.mx>
2025-04-06 22:48:32 -0600

    Vistas

--- Pokemon/app/models/pokemon_favorites.py
+++
@@ -1,6 +0,0 @@
-from app import mongo
-from app.models.super_clase import SuperClass
-
-class PokemonFavorites(SuperClass):
-   def __init__(self):
-        super().__init__("pokemon_favorites")
--- Pokemones/api/app/schemas/user_shema.py
+++ Pokemones/api/app/schemas/user_shema.py
@@ -1,26 +1,27 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
 
 class UserSchema(Schema):
     name = fields.Str(
         required = True,
-        validate =lambda x: len (x) > 0,
+        validate=lambda x: len(x) > 0,
         error_messages={
             "required": "El nombre es requerido"
-        }
+        } 
+    )
+
+    email= fields.Email(
+        required = True,
+        validate=lambda x: "@utma.edu.mx" in x,
+        error_messages={
+            "required": "La contraseña es requerida"
+        } 
     )
 
     password = fields.Str(
         required = True,
-        validate =lambda x: len (x) > 0,
-        error_messages={
-            "required": "La contraseña es requerida"
-        }
-    )
-
-    email = fields.Str(
-        required = True,
-        validate =lambda x: "utma.edu.mx" in x,
+        validate=lambda x: len(x) > 0,
         error_messages={
             "required": "El nombre es requerido"
-        }
+        } 
+        
     )
